14c5728b9d31c9344e9f526cce9ddaf2d63f70a4
AnaMariaFer <45061776+AnaMariaFer@users.noreply.github.com>
2018-11-15 15:08:03 +0100

    clase de test pelicula creada

---
+++ src/test/TestPelicula.java
@@ -0,0 +1,18 @@
+package test;
+
+import modelo.Pelicula;
+
+public class TestPelicula {
+	
+	public boolean checkRepeatFilm(Pelicula pelicula)
+	{
+		return true;
+		
+	}
+	
+	public boolean checkNullFilmm(Pelicula pelicula)
+	{
+		return true;
+	}
+
+}
