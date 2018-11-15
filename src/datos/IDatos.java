19a89941d4e7af9d8c2c69276eda8fb523af0d45
MVictoriaPov <45061787+MVictoriaPov@users.noreply.github.com>
2018-11-15 14:46:23 +0100

    interfaz datos

--- src/datos/IDatos.java
+++ src/datos/IDatos.java
@@ -1,5 +1,15 @@
 package datos;
 
 public interface IDatos {
+	
+	public void add(String nombre);
+	
+	public void delete(String nombre);
+	
+	public String read(String nombre);
+	
+	public void update(String nombre);
+	
+	
 
 }
