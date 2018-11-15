03a5bbfdbdea935a2d6b35cfd4f068f452441388
AnaMariaFer <45061776+AnaMariaFer@users.noreply.github.com>
2018-11-15 14:33:47 +0100

    Creada interfaz servicicios pelis

---
+++ src/servicios/ServiciosPeliculasImp.java
@@ -0,0 +1,28 @@
+package servicios;
+
+public class ServiciosPeliculasImp implements IServicios{
+
+	public boolean check(String nombre){
+		return false;
+		
+	}
+	
+	public boolean update(String nombre){
+		return false;
+		
+	}
+	
+	public boolean add(String nombre){
+		return false;
+	}
+	
+	public boolean delete(String nombre)
+	{
+		return false;
+	}
+	
+	public boolean read(String nombre){
+		return false;
+	}
+	
+}
