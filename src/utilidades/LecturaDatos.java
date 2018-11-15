625e214bc43b1443f40bb0f9dd2bbd9f5bcafb17
roarra <43341488+roarra@users.noreply.github.com>
2018-11-15 12:29:46 +0100

    Completamos la clase de lectura de datos con scanner

--- src/utilidades/LecturaDatos.java
+++ src/utilidades/LecturaDatos.java
@@ -1,5 +1,28 @@
 package utilidades;
 
+import java.util.Scanner;
+
 public class LecturaDatos {
+
+
+	    public static String leerString() {
+	        return new Scanner(System.in).nextLine();
+	    }
+
+	    public static String leerString(String msg) {
+	        System.out.println(msg);
+	        return leerString();
+	    }
+
+	    public static int leerInt() {
+	        return new Scanner(System.in).nextInt();
+	    }
+	    
+	        public static int leerInt(String msg) {
+	        System.out.println(msg);
+	        return leerInt();
+	    }
+	        
+	
 
 }
