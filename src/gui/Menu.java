2a43c783de3c5c514a5d69d964e6ee6e44dda34e
AngelMartinJimenez <45063649+AngelMartinJimenez@users.noreply.github.com>
2018-11-15 12:48:27 +0100

    Menu paquete gui

---
+++ src/gui/Menu.java
@@ -0,0 +1,35 @@
+package gui;
+
+public class Menu {
+	
+	public static void imprimeMenu(){
+		System.out.println("*--------------------------------------*");
+		System.out.println("|              -- PELICULAS --         |");
+		System.out.println("| 1. Cargar Peliculas                  |");
+		System.out.println("| 2. Añadir Pelicula                   |");
+		System.out.println("| 3. Listar Peliculas                  |");
+		System.out.println("| 4. Eliminar Peliculas                |");
+		System.out.println("| 5. Peliculas mas vistas              |");
+		System.out.println("| 6. Peliculas mas valoradas           |");
+		System.out.println("| 7. Que peliculas puedo ver           |");
+		System.out.println("| 8. Listado Peliculas por Categoria   |");
+		System.out.println("| 9. Buscar Pelicula                   |");
+		System.out.println("|--------------------------------------|");
+		System.out.println("|             -- USUARIOS --           |");
+		System.out.println("| 5. Añadir Usuario                    |");
+		System.out.println("| 6. Actualizar Usuario                |");
+		System.out.println("| 7. Listar Usuario                    |");
+		System.out.println("| 8. Eliminar Usuario                  |");
+		System.out.println("|--------------------------------------|");
+		System.out.println("|             -- CATEGORIA --          |");
+		System.out.println("| 5. Añadir Categoria                  |");
+		System.out.println("| 6. Actualizar Categoria              |");
+		System.out.println("| 7. Ver Categoria                     |");
+		System.out.println("| 8. Eliminar Categoria                |");
+		System.out.println("|--------------------------------------|");
+		System.out.println("| 0. Salir                             |");
+		System.out.println("*--------------------------------------*");
+		
+	}
+
+}
