49ca6b4a2eeddf12102f213fd8e217a2f1a7b5b3
roarra <43341488+roarra@users.noreply.github.com>
2018-11-15 14:57:11 +0100

    clase usuario imp realizada

--- src/datos/UsuariosImp.java
+++ src/datos/UsuariosImp.java
@@ -1,5 +1,21 @@
 package datos;
 
-public class UsuariosImp {
+public abstract class UsuariosImp implements IDatos {
 
+	public void add(String nombre){
+		
+	}
+	
+	public void delete(String nombre){
+		
+	}
+	
+	public String read(String nombre){
+		
+		return nombre;
+	}
+	
+	public void update(String nombre){
+		
+	}
 }
