4f8b6edb1d6485f3559da74c41abde4c447eeba1
AnaMariaFer <45061776+AnaMariaFer@users.noreply.github.com>
2018-11-17 14:28:25 +0100

    Prueba pruebas unitarias

---
+++ src/test/TestPeliculas.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestPeliculas {
+
+	@Test
+	public void CheckRepeatFilm() {
+		String resultadoReal = PeliculasImp.add("Humor");
+		
+	}
+
+}
